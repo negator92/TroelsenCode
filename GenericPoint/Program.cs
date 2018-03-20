5638793872d86b5732b62d5d667ff69d60e28973
Vincent Neparidze <nva@akmetron.ru>
2018-03-20 10:33:31 +0300

    Page354

--- GenericPoint/Program.cs
+++ GenericPoint/Program.cs
@@ -7,8 +7,15 @@ namespace GenericPoint
         static void Main(string[] args)
         {
             Point<int> intPoint = new Point<int>(20, 20);
+            Console.WriteLine($"intPoint.ToString() = {intPoint.ToString()}");
+            intPoint.ResetPoint();
+            Console.WriteLine($"intPoint.ToString() = {intPoint.ToString()}\n");
 
-            Point<double> doublePoint = new Point<double>(2.0, 0.2);
+
+            Point<double> doublePoint = new Point<double>(2.5, 8.8);
+            Console.WriteLine($"doublePoint.ToString() = {doublePoint.ToString()}");
+            doublePoint.ResetPoint();
+            Console.WriteLine($"doublePoint.ToString() = {doublePoint.ToString()}");
         }
     }
 }
