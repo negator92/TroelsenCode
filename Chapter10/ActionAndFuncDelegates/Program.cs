e02a80b2199c7e6ac7b3b6252b5657822b15288e
Vincent Neparidze <nva@akmetron.ru>
2018-04-03 11:36:32 +0300

    Page374

---
+++ Chapter10/ActionAndFuncDelegates/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ActionAndFuncDelegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string, ConsoleColor, int> actionTarget = new Action<string, ConsoleColor, int>(DisplayMessage);
+            actionTarget("Action message!", ConsoleColor.Cyan, 3);
+        }
+
+        static void DisplayMessage(string message, ConsoleColor textColor, int printCount)
+        {
+            ConsoleColor previousColor = Console.ForegroundColor;
+            Console.ForegroundColor = textColor;
+            for (int i = 0; i < printCount; i++)
+                Console.WriteLine(message);
+            Console.ForegroundColor = previousColor;
+        }
+    }
+}
