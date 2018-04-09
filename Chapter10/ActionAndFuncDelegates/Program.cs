9ffd24b59752227bf3e67de9d74c021f8243a77f
Vincent Neparidze <negator92@gmail.com>
2018-04-09 09:26:04 +0300

    Page375

--- Chapter10/ActionAndFuncDelegates/Program.cs
+++ Chapter10/ActionAndFuncDelegates/Program.cs
@@ -4,11 +4,23 @@ namespace ActionAndFuncDelegates
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Func<int, int, int> funcToInt = new Func<int, int, int>(Add);
+            int result = funcToInt.Invoke(20, 20);
+            Console.WriteLine($"20 + 20 = {result}");
+            Func<int, int, string> funcToString = SumToString;
+            string sum = funcToString(10, 300);
+            Console.WriteLine($"10 + 300 = {sum}");
             Action<string, ConsoleColor, int> actionTarget = new Action<string, ConsoleColor, int>(DisplayMessage);
             actionTarget("Action message!", ConsoleColor.Cyan, 3);
         }
+
+        static string SumToString(int x, int y)
+            => (x + y).ToString();
+
+        static int Add(int x, int y)
+            => x + y;
 
         static void DisplayMessage(string message, ConsoleColor textColor, int printCount)
         {
