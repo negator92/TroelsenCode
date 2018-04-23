859e554fa68fcc0b37b33c4b38d75bdc52b6d9f9
Vincent Neparidze <negator92@gmail.com>
2018-04-23 17:44:35 +0300

    Page395

---
+++ Chapter11/SimpleIndexer/PersonCollection.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SimpleIndexer
+{
+    public class PersonCollection : IEnumerable
+    {
+        private ArrayList arrayListPeople = new ArrayList();
+
+        public Person GetPerson(int position)
+            => (Person)arrayListPeople[position];
+
+        public void AddPerson(Person person)
+        {
+            arrayListPeople.Add(person);
+        }
+
+        public void ClearPeople()
+        {
+            arrayListPeople.Clear();
+        }
+
+        public int Count { get => arrayListPeople.Count; }
+        
+        // IEnumerator IEnumerable.GetEnumerator()
+        IEnumerator IEnumerable.GetEnumerator()
+            => arrayListPeople.GetEnumerator();
+    }
+}
---
+++ Chapter11/SimpleIndexer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SimpleIndexer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** Fun with Indexers ***\n");
+
+            PersonCollection people = new PersonCollection();
+
+            people[0] = new Person("Homer", "Simpson ", 40);
+            people[1] = new Person("Marge ", "Simpson ", 38);
+            people[2] = new Person(" Lisa ", " S impson", 9);
+            people[3] = new Person("Bart ", " Simpson", 7);
+            people[4] = new Person("Maggie ", "Simpson", 2);
+
+            for (int person = 0; person < people.Count; person++)
+            {
+                Console.WriteLine($"Person №{person}");
+                Console.WriteLine($"Name: {people[person].FirstName} {people[person].LastName}");
+                Console.WriteLine($"Age: {people[person].Age}");
+            }
+        }
+    }
+}
