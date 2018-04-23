df95a77de09416abbaac618ad95909f7793a2cf7
Vincent Neparidze <negator92@gmail.com>
2018-04-23 11:06:19 +0300

    Page394

---
+++ Chapter10/LambdaExpressionsMultipleParams/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LambdaExpressionsMultipleParams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleMath math = new SimpleMath();
+            math.SetMathHandler((message, result) =>
+                {
+                    Console.WriteLine($"Message: {message}, result: {result}");
+                });
+            math.Add(10, 10);
+        }
+    }
+}
---
+++ Chapter10/LambdaExpressionsMultipleParams/SimpleMath.cs
@@ -0,0 +1,15 @@
+﻿namespace LambdaExpressionsMultipleParams
+{
+    public class SimpleMath
+    {
+        public delegate void MathMessage(string message, int result);
+
+        private MathMessage mathMessageDelegate;
+
+        public void SetMathHandler(MathMessage target)
+            => mathMessageDelegate = target;
+
+        public void Add(int x, int y)
+            => mathMessageDelegate.Invoke("Adding has completed!", x + y);
+    }
+}
